7f01ecaafaaba49f5c9edd1d94d22cef2231a520
Raphaël Pinson <raphael.pinson@camptocamp.com>
2022-02-01 10:00:22 +0100

    refactor!: use var.cluster_info
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

--- modules/local.tf
+++ modules/local.tf
@@ -10,13 +10,13 @@ locals {
   )
 
   default_yaml = [ templatefile("${path.module}/values.tmpl.yaml", {
-      base_domain           = var.base_domain,
-      cluster_issuer        = var.cluster_issuer
-      oidc                  = var.oidc
+    base_domain           = var.cluster_info.base_domain
+    cluster_issuer        = var.cluster_issuer
+    oidc                  = var.oidc
 
-      argocd                = local.argocd
-      repositories          = var.repositories
-      server_admin_password = htpasswd_password.argocd_server_admin.bcrypt
+    argocd                = local.argocd
+    repositories          = var.repositories
+    server_admin_password = htpasswd_password.argocd_server_admin.bcrypt
   }) ]
   all_yaml = concat(local.default_yaml, var.extra_yaml)
 }
