6d8e2c3fc55ed64601ef01f26cbbabd8f5caa7ce
Xavier Rakotomamonjy <xavier.rakotomamonjy@camptocamp.com>
2022-07-22 11:06:02 +0200

    chore(boostrap): remove unused resources
    remove unused resource from the bootstrap. The resources were only used
    in output, or not use at all.

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -24,22 +24,6 @@ resource "random_password" "argocd_server_secretkey" {
   special = false
 }
 
-resource "jwt_hashed_token" "argocd" {
-  algorithm   = "HS256"
-  secret      = local.argocd_server_secretkey
-  claims_json = jsonencode(local.jwt_token_payload)
-}
-
-resource "random_password" "oauth2_cookie_secret" {
-  length  = 16
-  special = false
-}
-
-resource "random_password" "argocd_server_admin" {
-  length  = 16
-  special = false
-}
-
 resource "htpasswd_password" "argocd_server_admin" {
   password = random_password.argocd_server_admin.result
 }
--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,14 +1,3 @@
-output "argocd_auth_token" {
-  description = "The token to set in ARGOCD_AUTH_TOKEN environment variable."
-  value       = jwt_hashed_token.argocd.token
-}
-
-output "argocd_server_admin_password" {
-  description = "The ArgoCD admin password."
-  sensitive   = true
-  value       = random_password.argocd_server_admin.result
-}
-
 output "argocd_server_secretkey" {
   description = "The ArgoCD server secret key."
   sensitive   = true
