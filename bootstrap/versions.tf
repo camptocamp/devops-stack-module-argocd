4ce60de5d5797c247583173693a4a79e7feef1a2
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-03-08 09:36:50 +0100

    fix: change to looser versions constraints as per best practices
    See documentation [here](https://developer.hashicorp.com/terraform/language/providers/requirements#version-constraints).

--- bootstrap/versions.tf
+++ bootstrap/versions.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0"
+      version = ">= 2"
     }
     utils = {
       source = "cloudposse/utils"
-      version = "~> 1.6"
+      version = ">= 1.6"
     }
     htpasswd = {
       source = "loafoe/htpasswd"
-      version = "~> 0.9"
+      version = ">= 1"
     }
     jwt = {
       source  = "camptocamp/jwt"
-      version = "~> 1.1"
+      version = ">= 1.1"
     }
   }
   required_version = ">= 1.2"
--- versions.tf
+++ versions.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     argocd = {
       source  = "oboukili/argocd"
-      version = "~> 4.2"
+      version = ">= 4.2"
     }
     utils = {
       source  = "cloudposse/utils"
-      version = "~> 1.6"
+      version = ">= 1.6"
     }
     htpasswd = {
       source  = "loafoe/htpasswd"
-      version = "~> 0.9"
+      version = ">= 1"
     }
     jwt = {
       source  = "camptocamp/jwt"
-      version = "~> 1.1"
+      version = ">= 1.1"
     }
     time = {
       source  = "hashicorp/time"
-      version = "~> 0.9"
+      version = ">= 0.9"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = ">= 3"
     }
   }
   required_version = ">= 1.2"
