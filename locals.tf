4c2839d11b006724da1fc806100fdb7201db0de6
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-03-22 16:40:07 +0100

    fix: readd previous group for backwards compatibility

--- locals.tf
+++ locals.tf
@@ -12,6 +12,7 @@ locals {
           "policy.default" = ""
           "policy.csv"     = <<-EOT
                               g, pipeline, role:admin
+                              g, argocd-admin, role:admin
                               g, devops-stack-admins, role:admin
                             EOT
         }
