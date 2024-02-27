ae712db9ebbac8cc9fbf1fca5fc4a585b5d142ab
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2024-02-27 13:14:14 +0100

    fix: remove legacy ingress annotations
    The SSL redirection is no longer defined by these annotations, I think this is a leftover from ancient code. The HTTP -> HTTPS redirection is handled natively by the Traefik module and is enabled by default (a variable is available to deactivate it if necessary).

--- locals.tf
+++ locals.tf
@@ -248,8 +248,6 @@ locals {
             "cert-manager.io/cluster-issuer"                   = "${var.cluster_issuer}"
             "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
             "traefik.ingress.kubernetes.io/router.tls"         = "true"
-            "ingress.kubernetes.io/ssl-redirect"               = "true"
-            "kubernetes.io/ingress.allow-http"                 = "false"
           }
           hosts = [
             local.argocd_hostname_withclustername,
