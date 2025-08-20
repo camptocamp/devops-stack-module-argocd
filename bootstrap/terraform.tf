f8d92664b2316b11eeccb25f66c44c3daf835372
Lenny Consuegra <lenny.consuegra@camptocamp.com>
2025-08-20 09:31:37 +0200

    feat!: make version working with helm v3 provider
    https://github.com/hashicorp/terraform-provider-helm/blob/main/docs/guides/v3-upgrade-guide.md

--- bootstrap/terraform.tf
+++ bootstrap/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2"
+      version = ">= 3"
     }
     utils = {
       source  = "cloudposse/utils"
